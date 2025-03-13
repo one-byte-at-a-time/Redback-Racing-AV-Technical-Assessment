b4ff935ce3ce6e038657a9f59c4feed29bec14a3
one-byte-at-a-time <158281436+one-byte-at-a-time@users.noreply.github.com>
2025-03-13 15:37:28 +1100

    progression on Q1

--- Question-1/Question-1.cc
+++ Question-1/Question-1.cc
@@ -7,6 +7,7 @@
 #include <map>
 #include <vector>
 #include <initializer_list>
+#include <cmath>
 
 const int values[] = { 1,2,3,4,5 };
 const int val_size = sizeof values / sizeof(int);
@@ -14,21 +15,24 @@ const int val_size = sizeof values / sizeof(int);
 
 
 class Foo {
+
     std::vector<int> mElements;
-public:
-    Foo(std::initializer_list<int> list)
-    : mElements(list) {}
+
+    public:
+        Foo(std::initializer_list<int> list)
+        : mElements(list) {}
 };
 
-struct {
+
+struct person {
     int age;
     float weight;
-} person;
+};
 
 
 void show_output(const int** pp)
 {
-    printf("%p : %p : %d", pp, *pp, **pp);
+    std::cout << pp << " : " << *pp << " : " << **pp << std::endl;
 }
 
 
@@ -37,50 +41,58 @@ int main() {
 
     int x = -10;
     int y = 15;
-    cout << " " << (x,y) << std::endl;
+    // NB: im assuming this is what they want?
+    std::cout << " " << "(" << x << "," << y << ")" << std::endl;
 
     // print integer ratios of y:x till x < y
     // invalid ratios should print 0
     while (x < y)
     {
-        cout << "ratio: " << (y/x) << endl;
+        std::cout << "ratio: " << y/x << std::endl;
         x++;
         y--;
     }
 
 
     int i = 1, j = 1; // don't change values
+
     if ((i = !3) & (j = 1))
     {
-        cout << "j is 1 and i is not 3\n";
+        std::cout << "j is 1 and i is not 3\n";
     }
     else {
-        cout << "either j is NOT 1, or i is set to 3\n";
+        std::cout << "either j is NOT 1, or i is set to 3\n";
 
     }
 
 
-    typedef map<int, double> valmap;
+    typedef std::map<int, double> valmap;
     valmap m;
 
-    for (int i = 0; i < valSize; i++)
-        m.insert(make_pair(values[i], pow(values[i], .5)));
+    for (int i = 0; i < val_size; i++)
+        m.insert(std::make_pair(values[i], pow(values[i], .5)));
 
     m.insert(1, 2);
 
 
-    int n = 1;
-    int* p = &n;
-    show_output(p);
+    // NB: seems a little silly?
+    const int n = 1;
+    const int* p = &n;
+    const int** pp = &p;
+    show_output(pp);
 
     // Initialise a person on the heap with the use of smart pointers (unique_ptr) instead.
     struct person* ptr;
-    ptr = (struct person*)malloc(sizeof(struct person));
+    // This is too c, not cpp:
+    // ptr = (struct person*)malloc(sizeof(struct person));
+    ptr = new person;
     ptr->age = 10;
     ptr->weight = 55.5;
 
     // Initialise with 5 integers
-    Foo foo;
+    // Foo foo;
+    std::initializer_list<int> list = {1,2,3,4,5};
+    Foo foo = new Foo(list);
 
     return 0;
 }
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/riluk/repos/Redback-Racing-AV-Technical-Assessment
+# Build directory: /home/riluk/repos/Redback-Racing-AV-Technical-Assessment/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(question-1 "Question-1-out")
+set_tests_properties(question-1 PROPERTIES  _BACKTRACE_TRIPLES "/home/riluk/repos/Redback-Racing-AV-Technical-Assessment/CMakeLists.txt;13;add_test;/home/riluk/repos/Redback-Racing-AV-Technical-Assessment/CMakeLists.txt;0;")
+add_test(question-2 "Question-2-out")
+set_tests_properties(question-2 PROPERTIES  _BACKTRACE_TRIPLES "/home/riluk/repos/Redback-Racing-AV-Technical-Assessment/CMakeLists.txt;13;add_test;/home/riluk/repos/Redback-Racing-AV-Technical-Assessment/CMakeLists.txt;0;")
+add_test(question-3 "Question-3-out")
+set_tests_properties(question-3 PROPERTIES  _BACKTRACE_TRIPLES "/home/riluk/repos/Redback-Racing-AV-Technical-Assessment/CMakeLists.txt;13;add_test;/home/riluk/repos/Redback-Racing-AV-Technical-Assessment/CMakeLists.txt;0;")
